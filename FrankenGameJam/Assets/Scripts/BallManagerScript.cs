c58f8c9eb1fdb53fbdb516814eda7a19987fc2ad
Tobbes <Tobias.Sybel@gmx.de>
2019-06-28 23:55:38 +0200

    WIP

---
+++ FrankenGameJam/Assets/Scripts/BallManagerScript.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallManagerScript : MonoBehaviour
+{
+    public List<GameObject> ballList = new List<GameObject>();
+    public List<BallSpawnScript> spawnerList = new List<BallSpawnScript>();
+    public float timeCounter;
+    public float maxTimeCounter;
+    public int maxBalls;
+    public int deadBalls;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        timeCounter = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (ballList.Count >= maxBalls + deadBalls/2) return;
+        timeCounter += Time.deltaTime;
+        if (timeCounter >= maxTimeCounter)
+        {
+            BallSpawn();
+            timeCounter = 0;
+        }
+    }
+
+    public void BallSpawn()
+    {
+        var rng = Random.Range(0, spawnerList.Count);
+        ballList.Add(spawnerList[rng].SpawnBall(this));
+        
+    }
+
+    public void BallRemove(GameObject ball)
+    {
+        if (ballList.Contains(ball))
+        {
+            ballList.Remove(ball);
+            deadBalls++;
+        }
+    }
+       
+    //ballCounter.add /ballCounter.remove
+}
---
+++ FrankenGameJam/Assets/Scripts/BallScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallScript : MonoBehaviour
+{
+    public BallManagerScript managerScript;
+
+    public void HitPlayer()
+    {
+        
+    }
+
+    public void OnTriggerEnter2D (Collider2D col)
+    {
+        if(col.gameObject.tag == "BallBorder")
+        {
+            managerScript.BallRemove(gameObject);
+            Destroy(gameObject);
+        }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
