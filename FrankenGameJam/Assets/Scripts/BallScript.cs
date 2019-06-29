a3a3bc0c663a6ccfbaccb494b83152b506899a09
Peco0o <jan.raumanns@arcor.de>
2019-06-29 18:22:26 +0200

    INTHEFAAACE

--- FrankenGameJam/Assets/Scripts/BallScript.cs
+++ FrankenGameJam/Assets/Scripts/BallScript.cs
@@ -8,6 +8,7 @@ public class BallScript : MonoBehaviour
     public Coroutine looseHealthRoutine;
     public float timer;
 
+
     public void OnTriggerEnter2D (Collider2D col)
     {
         if(col.CompareTag("BallBorder"))
@@ -27,28 +28,42 @@ public class BallScript : MonoBehaviour
     
     public IEnumerator LooseHealthRoutine(GameObject player)
     {
-       Movement movescript = player.GetComponent<Movement>();
-       while(timer > 0)
-        {
-            if (movescript.playertype == Playertype.PLAYER1)
+        Movement movescript = player.GetComponent<Movement>();
+        movescript.playerhalo.enabled = true;
+
+        if (movescript.playertype == Playertype.PLAYER1)
             {
-                if (Input.GetButtonDown("CatchP1"))
+                while(timer > 0)
                 {
+ 
+                    if (Input.GetButtonDown("CatchP1"))
+                    {
+                    movescript.playerhalo.enabled = false;
                     break;
+                    }
+                    timer -= Time.deltaTime;
+                    yield return null;
                 }
             }
 
             if (movescript.playertype == Playertype.PLAYER2)
             {
+                while(timer > 0)
+                {
 
+                    if (Input.GetButtonDown("CatchP2"))
+                    {
+                    movescript.playerhalo.enabled = false;
+                    break;
+                    }
+                    timer -= Time.deltaTime;
+                    yield return null;
+                }
             }
-            timer = timer - Time.deltaTime;
-            yield return null;
-        }
 
         if (timer <= 0)
         {
-
+            movescript.playerhalo.enabled = false;
             player.GetComponent<Health>().Loosehealth();
             yield return null;
         }
---
+++ FrankenGameJam/Assets/Scripts/INTHEFAAACE.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class INTHEFAAACE : MonoBehaviour
+{
+    public GameObject ballPrefab;
+    public float movespeedBall;
+    public Rigidbody2D body;
+    public Coroutine fireroutine;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            if(fireroutine == null)
+            {
+                fireroutine = StartCoroutine(Fire());
+            }
+        }
+    }
+
+    public IEnumerator Fire()
+    {
+        Movement movescript = GetComponent<Movement>();
+        BallSpawnScript ballscript = GetComponent<BallSpawnScript>();
+        
+            var ball = Instantiate(ballPrefab, transform.position, Quaternion.identity);
+            var rbBall = ball.GetComponent<Rigidbody2D>();
+            rbBall.AddForce((body.velocity.magnitude + movespeedBall) * new Vector2(movescript.looking.x,movescript.looking.y * -1),ForceMode2D.Impulse);
+            Debug.Log("Fire!");
+        yield return new WaitForSeconds(2);
+        fireroutine = null;
+        
+    }
+    
+}
