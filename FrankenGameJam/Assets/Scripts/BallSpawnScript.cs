cbe30e2927f5e3da89b6a3d76769326ae559ac4d
Nic <nicolas@raveyard.de>
2019-06-29 22:53:37 +0200

    added stuffs

--- FrankenGameJam/Assets/Scripts/BallSpawnScript.cs
+++ FrankenGameJam/Assets/Scripts/BallSpawnScript.cs
@@ -16,28 +16,6 @@ public class BallSpawnScript : MonoBehaviour
     public float movespeedBall;
     public ShootingDirection shootingDirection;
 
-    public int randomize;
-    public GameObject ball1;
-    public GameObject ball2;
-    public GameObject ball3;
-
-    void FixedUpdate()
-    {
-        randomize = Random.Range(0,3);
-        if (randomize == 0)
-        {
-            ballPrefab = ball1;
-        }
-        if (randomize == 1)
-        {
-            ballPrefab = ball2;
-        }
-        if (randomize == 2)
-        {
-            ballPrefab = ball3;
-        }
-    }
-
     public GameObject SpawnBall(BallManagerScript manager)
     {
         var ball = Instantiate(ballPrefab, transform.position, Quaternion.identity);
