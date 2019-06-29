548fbd570ab6f338382fc94c4b9022e9f57406ec
Nic <nicolas@raveyard.de>
2019-06-29 19:33:25 +0200

    thrown ball wip

---
+++ FrankenGameJam/Assets/PlayerThrownBall.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerThrownBall : GenericBall
+{
+
+    public GameObject origin;
+    public Coroutine looseHealthRoutine;
+    public float timer;
+
+    public void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.CompareTag("BallBorder"))
+        {
+            managerScript.BallRemove(gameObject);
+            Destroy(gameObject);
+        }
+        if (col.CompareTag("Player") && col.gameObject != origin)
+        {
+            if (looseHealthRoutine == null)
+            {
+                looseHealthRoutine = StartCoroutine(LooseHealthRoutine(col.gameObject));
+            }
+            managerScript.BallRemove(gameObject);
+        }
+    }
+
+
+    public IEnumerator LooseHealthRoutine(GameObject player)
+    {
+        Movement movescript = player.GetComponent<Movement>();
+        player.GetComponent<Health>().Loosehealth();
+        yield return null;
+        Destroy(gameObject);
+    }
+}
